0087a8f47f9f927ed2af0a14ce31d4966693ccd5
motola <olutolamoses@gmailcom>
2022-09-03 20:39:15 +0100

    index on jwt: dd0b927 Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors")
+require("dotenv").config;
+const bycrpt = require("bcrypt"); 
+const jwtRouter = require('./routers/route');
+
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(jwtRouter);
+
+
+const port = process.env.PORT || 3000
+
+
+
+app.get('', (req,res) => {
+    res.send("Working on JWT")
+
+})
+
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+});
---
+++ routers/route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { login, dashboard } = require('../controllers/main');
+const { authMiddleware } = require('../middleware/auth')
+
+const router = express.Router();
+
+
+router.route('/login').post(login)
+router.route('/dashboard').get(authMiddleware, dashboard);
+
+
+module.exports = router
